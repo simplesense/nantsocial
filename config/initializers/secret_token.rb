f7a1ec25bee8c67f43fee9403a767de0d7f549e2
Satya <satya@satya.me>
2014-01-30 11:49:36 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nantsocial::Application.config.secret_key_base = 'e98cfcc136196baa1b8dd160e85bcce95eb657e6fc05f36e3dc7b226b9b84a7849d33052bca5d25f4fb32412227098544552035c072cef791257f078b507ed1c'
