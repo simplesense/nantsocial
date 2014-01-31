a53dab9873d9c98f32cf7a18b02bcbb00f461825
Satya <satya@satya.me>
2014-01-30 21:46:33 -0600

    generating-users-with-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Nantsocial::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
 
