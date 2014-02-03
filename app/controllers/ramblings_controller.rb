0ac9d87b8bc865608372cd87bd5c91bbf881a793
Satya <satya@satya.me>
2014-02-03 13:16:21 -0600

    added pagination

--- app/controllers/ramblings_controller.rb
+++ app/controllers/ramblings_controller.rb
@@ -4,7 +4,7 @@ class RamblingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @ramblings = Rambling.all.order("created_at DESC").paginate(:page => params[:page])
+    @ramblings = Rambling.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -103,7 +103,7 @@ Devise.setup do |config|
   # able to access the website for two days without confirming his account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming his account.
-  # config.allow_unconfirmed_access_for = 2.days
+  config.allow_unconfirmed_access_for = 0.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
@@ -135,7 +135,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 8..128.
-  config.password_length = 8..128
+  config.password_length = 4..20
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
