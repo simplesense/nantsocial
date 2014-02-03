d4a347f4e507580b8aaa3e8f50e860cf8fadc5fc
Satya <satya@satya.me>
2014-02-02 23:00:03 -0600

    glyphicons

--- app/models/rambling.rb
+++ app/models/rambling.rb
@@ -1,5 +1,5 @@
 class Rambling < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "450x450>", :thumb => "150x150>" }, :default_url => "http://uk.virginmoneygiving.com/fundraiser-web/img/branding/no-profile-image.png"
+	has_attached_file :image, :styles => { :large => "800x800", :medium => "450x450>", :thumb => "150x150>" }, :default_url => "http://uk.virginmoneygiving.com/fundraiser-web/img/branding/no-profile-image.png"
 end
