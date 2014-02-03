681e52626bc6c9c9fcf9abd723051b4458ae921b
Satya <satya@satya.me>
2014-02-02 21:44:13 -0600

    styling-with-jquery-masonry-pinteresting

--- app/models/rambling.rb
+++ app/models/rambling.rb
@@ -1,5 +1,5 @@
 class Rambling < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://uk.virginmoneygiving.com/fundraiser-web/img/branding/no-profile-image.png"
+	has_attached_file :image, :styles => { :medium => "450x450>", :thumb => "150x150>" }, :default_url => "http://uk.virginmoneygiving.com/fundraiser-web/img/branding/no-profile-image.png"
 end
